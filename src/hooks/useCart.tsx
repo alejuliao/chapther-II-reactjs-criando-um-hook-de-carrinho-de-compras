0514ac50207fed5bd202b6022e937f107d89a554
alejuliao <alexandrejuliao@icloud.com>
2022-05-29 12:30:18 -0300

    add cart OK

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,11 +23,11 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
@@ -35,14 +35,32 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const addProduct = async (productId: number) => {
     try {
       // TODO
+      // console.log(cart)
+      const productInCart = cart.find(product => product.id === productId)
+      const stock = await api.get(`stock/${productId}`).then(response =>response.data )
+      const product = await api.get(`products/${productId}`).then(response =>response.data )
+
+      if(!productInCart){
+        if(stock.amount >1){
+          setCart([...cart, {...product, amount:1}])
+          return
+        }
+      }
+      if(productInCart && stock.amount > productInCart.amount){
+        const newCart = cart.map(item => item.id === productId 
+          ? {...item, amount: Number(item.amount) +1} 
+        : item)
+        setCart(newCart)
+      }
     } catch {
       // TODO
     }
   };
-
+  
   const removeProduct = (productId: number) => {
     try {
       // TODO
+      console.log('remove',productId)
     } catch {
       // TODO
     }
@@ -54,6 +72,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   }: UpdateProductAmount) => {
     try {
       // TODO
+      const {data} = await api.get('stock')
+      console.log(data)
+
     } catch {
       // TODO
     }
